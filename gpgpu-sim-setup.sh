eabdd4dd683859b48852373d1e16c73e99c6032c
hien <hienvu.vg@gmail.com>
2022-12-06 10:57:54 -0600

    sh

---
+++ gpgpu-sim-setup.sh
@@ -0,0 +1,18 @@
+# @Author: hien
+# @Date:   2022-12-06 10:56:42
+# @Last Modified by:   hien
+# @Last Modified time: 2022-12-06 10:57:13
+
+cd ~/gpgpu-sim_distribution
+make clean
+source setup_environment
+make
+
+rm -rf ~/test
+mkdir ~/test # make directory at root
+cp configs/GTX480/* ~/test/. # copy GTX480 configuration from gpgpu-sim to test
+
+cd ~/ispass2009-benchmarks
+./setup_config.sh --cleanup
+./setup_config.sh 
+make -f Makefile.ispass-2009 
