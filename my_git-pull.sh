1fd6017f282320201f224cc8d7613e97673f8697
hien <hienvu.vg@gmail.com>
2022-12-06 11:37:03 -0600

    4-way

---
+++ my_git-pull.sh
@@ -0,0 +1,8 @@
+# @Author: hien
+# @Date:   2022-12-06 10:56:42
+# @Last Modified by:   hien
+# @Last Modified time: 2022-12-06 11:36:30
+
+git fetch origin main
+git reset --hard origin/main
+git pull origin main
