fc53a4b6294b65e22c0e2d45b0c8a4b855c69214
hien <hienvu.vg@gmail.com>
2022-12-06 11:14:11 -0600

    git_pull

--- git_pull.sh
+++ git_pull.sh
@@ -1,18 +1,8 @@
 # @Author: hien
 # @Date:   2022-12-06 10:56:42
 # @Last Modified by:   hien
-# @Last Modified time: 2022-12-06 10:57:13
+# @Last Modified time: 2022-12-06 11:12:36
 
-cd ~/gpgpu-sim_distribution
-make clean
-source setup_environment
-make
-
-rm -rf ~/test
-mkdir ~/test # make directory at root
-cp configs/GTX480/* ~/test/. # copy GTX480 configuration from gpgpu-sim to test
-
-cd ~/ispass2009-benchmarks
-./setup_config.sh --cleanup
-./setup_config.sh 
-make -f Makefile.ispass-2009 
+git fetch origin main
+git reset --hard origin/main
+git pull origin main
