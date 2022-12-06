a9f61fa377f9bb1fbcfccdc4c4dcc6307ffeca7a
hien <hienvu.vg@gmail.com>
2022-12-06 11:15:55 -0600

    del pull

--- git_pull.sh
+++
@@ -1,8 +0,0 @@
-# @Author: hien
-# @Date:   2022-12-06 10:56:42
-# @Last Modified by:   hien
-# @Last Modified time: 2022-12-06 11:12:36
-
-git fetch origin main
-git reset --hard origin/main
-git pull origin main
